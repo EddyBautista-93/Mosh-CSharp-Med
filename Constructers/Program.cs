9bd81775a2b25ac6d4e958f4f84faa0b0cd58780
EddyBautista <bautista.j.eddy@gmail.com>
2021-08-09 05:46:56 -0500

    set up construcer

---
+++ Constructers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructers
+{
+    class Program
+    {
+        public class Customer
+        {
+            public Customer()
+            {
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
